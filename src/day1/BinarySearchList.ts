34f679f9700344ef44858040f1c5b28a8eef7e3e
Dallin Christensen <christensen.dallin@gmail.com>
2023-10-21 22:19:35 -0700

    search algos

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,21 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+  let low = 0
+  let high = haystack.length
 
+  const midFind = (l: number, h: number) => Math.floor(((h-l)/2) + l)
+
+  do {
+    const m = midFind(low, high)
+    const v = haystack[m]
+
+    if (v === needle) {
+      return true
+    } else if (v > needle) {
+      high = m
+    } else {
+      low = m + 1
+    }
+  } while (low < high);
+
+  return false
 }
--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,19 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
 
+  const jumpAmount = Math.floor(Math.sqrt(breaks.length))
+
+  let i = jumpAmount
+  for (;i < breaks.length; i++) {
+    if (breaks[i]) {
+      break;
+    }
+  }
+
+  for(let j = i; j < i + jumpAmount; j ++) {
+    if (breaks[j]) {
+      return j
+    }
+  }
+
+  return -1
 }
